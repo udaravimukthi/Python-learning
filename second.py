f9c63012372ae5d4020c5eafcd5d503b926da270
udara <uvlakshan@gmail.com>
2020-10-17 21:01:03 +0530

    if else statements tried

---
+++ second.py
@@ -0,0 +1,18 @@
+a=10
+if a>45:
+    print(a)
+else:
+    print("false")
+
+b=15
+
+if b>10:
+    b=b*2
+    print (b)
+    
+elif b>5:
+    print("b")
+    
+else:
+    print("B is not greater than 10")
+
