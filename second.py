5b9ac66d0410c9c7721bd21f8fb3e8dfeca1c081
udara <uvlakshan@gmail.com>
2020-10-18 23:55:30 +0530

    changed second

--- second.py
+++ second.py
@@ -9,10 +9,20 @@ b=15
 if b>10:
     b=b*2
     print (b)
-    
+
 elif b>5:
     print("b")
     
 else:
     print("B is not greater than 10")
+
+d=6
+
+if d>5:
+    if d==6:
+        print("six")
+    else:
+        print("else")
+
+
 
