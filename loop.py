eefa56607221633885a8564bfd72aed6c6af8fd1
udara <uvlakshan@gmail.com>
2020-10-19 22:05:17 +0530

    added nested for loop

--- loop.py
+++ loop.py
@@ -16,4 +16,15 @@ print(y)
 for var in list(range(10)):
     print(var)
 
+for var in 'Udara':
+    print(var)
 
+for var in range(2,6):
+    print(var)
+
+for var in range(2,60,3):
+    print(var)
+
+for i in range(1,11):
+    for j in range(1,11):
+        print("i=",i,"j",j)
